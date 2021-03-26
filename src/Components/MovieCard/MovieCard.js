437c2714d38aea6ee93b094ce0b06069a0a69d91
abdelhak-touil <abdelhak.touil1994@gmail.com>
2021-03-26 17:19:53 +0100

    app movie is done

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -2,21 +2,19 @@ import React from "react";
 import { Card, Button } from "react-bootstrap";
 import "./MovieCard.css";
 
-function movieCard({ movie }) {
+function MovieCard({ movie }) {
   return (
-    <Card style={{ width: "18rem", margin: "10px" }}>
-      <Card.Img variant="top" src={movie.posterUrl} height="60%" />
+    <Card className="movieCard">
+      <Card.Img variant="top" src={movie.posterUrl} className="MovieImg" />
       <Card.Body>
         <Card.Title>{movie.title}</Card.Title>
         <Card.Text>{movie.description}</Card.Text>
+        <h2>{"⭐".repeat(movie.rate)}</h2>
       </Card.Body>
       <Card.Footer>
-        <Button variant="primary" className="movieButton">
-          Enjoy It
-        </Button>
+        <Button variant="primary">Download</Button>
       </Card.Footer>
     </Card>
   );
 }
-
-export default movieCard;
+export default MovieCard;
